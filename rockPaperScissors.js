3212284ec882f5e734b58a7787775591fae78cd9
JamarJ-Jam <225747219+JamarJ-Jam@users.noreply.github.com>
2025-09-02 00:16:16 -0500

    wrote the code for the play round function.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,13 +1,13 @@
 function getHumanChoice() {
     const userInput = prompt("Let's play a game!:");
-    const result = ("userInput:", userInput)
-    return result
+    const move = ("userInput:", userInput)
+    return move
 }
 console.log(getHumanChoice())
 
 
 function getComputerChoice() {
-    const words = ["rock", "paper", "scissors"]
+    const words = ['rock', 'paper', 'scissors']
 
     const randomIndex = Math.floor(Math.random() * 3);
 
@@ -18,4 +18,42 @@ function getComputerChoice() {
 
 console.log(getComputerChoice())
 
+let humanScore = 0;
+let computerScore = 0;
+const choices = ['rock', 'paper', 'scissors'];
 
+function playRound(humanChoice, computerChoice) {
+    const humanChoice = prompt("Enter 'rock', 'paper', or 'scissors':");
+    if (humanChoice === null) {
+        return "Try again!"
+    }
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    let result = "";
+    let winMessage = "";
+
+    if (humanChoice === computerChoice) {
+        result = "It's a tie!";
+        winMessage = "You both chose " + humanChoice + ".";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result = "You Win!";
+        winMessage = humanChoice + " beats " + computerChoice + ".";
+        humanScore++;
+    } else {
+        result = "You Lose!";
+        winMessage = computerChoice + " beats" + humanChoice + ".";
+        computerScore++;
+    }
+
+    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
