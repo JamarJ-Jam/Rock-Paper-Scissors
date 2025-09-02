f640b0dcf07c9cfa3689329026046ecda7060feb
JamarJ-Jam <225747219+JamarJ-Jam@users.noreply.github.com>
2025-09-02 01:29:58 -0500

    Added comments.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,52 +1,33 @@
-function getHumanChoice() {
-    const userInput = prompt("Let's play a game!:");
-    const move = ("userInput:", userInput)
-    return move
-}
-console.log(getHumanChoice())
-
-
-function getComputerChoice() {
-    const words = ['rock', 'paper', 'scissors']
-
-    const randomIndex = Math.floor(Math.random() * 3);
-
-    const randomWord = words[randomIndex];
-
-    return randomWord;
-}
-
-console.log(getComputerChoice())
-
+/*None of this is applying to the game*/
 let humanScore = 0;
 let computerScore = 0;
 const choices = ['rock', 'paper', 'scissors'];
 
-function playRound(humanChoice, computerChoice) {
-    const humanChoice = prompt("Enter 'rock', 'paper', or 'scissors':");
-    if (humanChoice === null) {
+function playRound(humanMove, computerMove) {
+    humanMove = prompt("Enter 'rock', 'paper', or 'scissors':");
+    if (humanMove === null) {
         return "Try again!"
     }
 
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    computerMove = choices[Math.floor(Math.random() * 3)];
 
     let result = "";
     let winMessage = "";
 
-    if (humanChoice === computerChoice) {
+    if (humanMove === computerMove) {
         result = "It's a tie!";
-        winMessage = "You both chose " + humanChoice + ".";
+        winMessage = "You both chose " + humanMove + ".";
     } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")
+        (humanMove === "rock" && computerMove === "scissors") ||
+        (humanMove === "paper" && computerMove === "rock") ||
+        (humanMove === "scissors" && computerMove === "paper")
     ) {
         result = "You Win!";
-        winMessage = humanChoice + " beats " + computerChoice + ".";
+        winMessage = humanMove + " beats " + computerMove + ".";
         humanScore++;
     } else {
         result = "You Lose!";
-        winMessage = computerChoice + " beats" + humanChoice + ".";
+        winMessage = computerMove + " beats" + humanMove + ".";
         computerScore++;
     }
 
@@ -57,3 +38,27 @@ const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+
+
+/*This works as expected*/
+function getHumanChoice() {
+    const userInput = prompt("Let's play a game!:");
+    const move = ("Player:" + " " + userInput);
+    const play = move.toLowerCase();
+    return play;
+}
+console.log(getHumanChoice())
+
+
+function getComputerChoice() {
+    
+    const randomIndex = Math.floor(Math.random() * 3);
+
+    const randomWord = "computer:" + " " + choices[randomIndex];
+
+    return randomWord;
+}
+
+console.log(getComputerChoice())
+
+
